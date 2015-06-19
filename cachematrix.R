ea69ebb74011adb883d0a28e59503516a4a7d5db
mssmani <testuser90210@hotmail.com>
2015-06-19 19:34:51 +0530

    This is the completed assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching a matrix inverse helps reduce the computational effort
+## This R program has a pair of functions (makeCacheMatrix, cacheSolve) to cache the inverse of a matrix
 
-## Write a short comment describing this function
+## This function (makeCacheMatrix) creates a special "matrix" object that can cache its inverse
+## This function uses the solve function in R to compute the inverse of the matrix
+## This function does the following
+## 1. set the value of the matrix
+## 2. get the value of the matrix
+## 3. set the value of inverse of the matrix
+## 4. get the value of the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) m <<- solve
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
+}
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above 
+## If the inverse has already been calculated (and the matrix has not changed), then cacheSolve will retrieve the inverse from the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## This will return a matrix that is the inverse of 'x'
+        ## This will check to see  if the  inverse has already been calculated and that the matrix has not  change
+        ## This  will then retrieve  the inverse from the cache
+        
+        m <- x$getinverse()
+	      if(!is.null(m)) {
+	              message("getting cached data")
+	              return(m)
+	      }
+	      data <- x$get()
+	      m <- solve(data, ...)
+	      x$setsolve(m)
+        m
 }
